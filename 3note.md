d4f8af381bc9031c8a27818e00de4998a41a1491
KanaMeisa <stewartjobs@outlook.com>
2023-04-24 15:20:34 +0800

    update

---
+++ 3note.md
@@ -0,0 +1,46 @@
+# 转专业复习笔记
+
+在C++中，有一些常见的算法是值得掌握的。这些算法并不限于C++，它们在计算机科学和编程领域都十分关键。以下是一些必会的算法：
+
+排序算法：
+
+- 冒泡排序（Bubble Sort）
+- 选择排序（Selection Sort）
+- 插入排序（Insertion Sort）
+- 快速排序（Quick Sort）
+- 归并排序（Merge Sort）
+- 堆排序（Heap Sort）
+  
+查找算法：
+
+- 线性查找（Linear Search）
+- 二分查找（Binary Search）
+
+图算法：
+
+- 深度优先搜索（DFS, Depth-First Search）
+- 广度优先搜索（BFS, Breadth-First Search）
+- 短路径（如Dijkstra算法）
+- 最小生成树（如Kruskal算法、Prim算法）
+  
+字符串匹配算法：
+
+- 朴素字符串匹配（Naive String Matching）
+- KMP算法（Knuth-Morris-Pratt Algorithm）
+  
+动态规划（Dynamic Programming）：
+
+- 背包问题（Knapsack Problem）
+- 最长公共子序列（Longest Common Subsequence）
+- 最长递增子序列（Longest Increasing Subsequence）
+
+分治算法（Divide and Conquer）：
+
+- 快速幂运算（Fast Exponentiation）
+- Strassen矩阵乘法（Strassen's Matrix Multiplication）
+
+贪心算法（Greedy Algorithm）：
+
+- 霍夫曼编码（Huffman Coding）
+- 最小生成树（如Kruskal算法、Prim算法）
+- 任务调度问题（Task Scheduling Problem）
--- Code/test.cpp
+++ Code/test.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
-#include<cstring>
-#include<string.h>
+#include <cstring>
 using namespace std;
 
 void strcpy_s(char* dest, size_t dest_size, const char* src) {
@@ -19,48 +18,7 @@ void strcpy_s(char* dest, size_t dest_size, const char* src) {
     strcpy(dest, src);
 }
 
-class Date;
-class Time{
-    private:
-        int hour;
-        int minute;
-        int second;
-
-    public:
-        Time(int hour, int minute, int second){
-            this -> hour = hour;
-            this -> minute = minute;
-            this -> second = second;
-        }
-
-    friend class Date;
-};
-
-class Date //定义日期类
-{
-    private:
-        int year;
-        int month;
-        int day;
-        Time t;
-
-    public:
-        Date(int y, int m, int d,int h,int mi,int s):t(h,mi,s){
-            year = y;
-            month = m;
-            day = d;
-        }
-
-        void showDate(){
-            cout << "当前日期为: ";
-            cout << year << "-" << month << "-" << day <<"\t";
-            cout << t.hour << "时" << t.minute << "分" << t.second << "秒"<< endl;//访问私有成员
-        }
-};
-
-
 int main(){
-    Date date1(2019,8,10,11,56,35);
-    date1.showDate();
+    cout << "hello, world" << endl;
     return 0;
 }
