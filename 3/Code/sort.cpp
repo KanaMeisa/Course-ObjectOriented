03ff6ce2e62856c113c8f4923a4c73180c4ae420
KanaMeisa <stewartjobs@outlook.com>
2023-04-24 21:58:05 +0800

    update sort notes

--- 3/Code/sort.cpp
+++ 3/Code/sort.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 using namespace std;
 
-// void bubbleSort(int arr[], int n) {
-//     for (int i = 0; i < n - 1; i ++) {
-//         for (int j = 0; j < n - i - 1; j ++) {
-//             if (arr[j] > arr[j + 1]) {
-//                 int temp = arr[j];
-//                 arr[j] = arr[j + 1];
-//                 arr[j + 1] = temp;
-//             }
-//         }
-//     } 
-// }
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i ++) {
+        for (int j = 0; j < n - i - 1; j ++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    } 
+}
 
 
 void selectionSort(int arr[], int n) {
@@ -32,10 +32,31 @@ void selectionSort(int arr[], int n) {
 }
 
 
+void insertionSort(int arr[], int n) {
+    int i, key, j;
+
+    for (i = 1; i < n; i++) {
+        key = arr[i];   //选取未排列序列中的第一个元素
+        j = i - 1;      //扫描从i前面一个元素开始
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];    //如果已经是正确的顺序，就将各元素右移一位
+            j = j - 1;
+        }
+
+        //循环结束。比如是在i处结束的循环，那么，i+1位置上的元素已经被移到i+2的位置上去了
+        //是空位，而i又比key小，i+2比key大，所以可以把key插入到i+1的位置上
+        arr[j + 1] = key;
+    } 
+}
+
+
+
+
 int main(){
     int a[] = {3, 2, 4, 5, 1, 8, 7, 6};
     int n = sizeof(a) / sizeof(a[0]);
-    selectionSort(a, n);
+    insertionSort(a, n);
 
     for (int i = 0; i < n; i ++){
         cout << a[i] << endl;
