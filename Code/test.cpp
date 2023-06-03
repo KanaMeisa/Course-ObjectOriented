dc86e205e3fe8a22fea24c267465ec237d74fbce
KanaMeisa <stewartjobs@outlook.com>
2023-06-03 18:23:59 +0800

    changes

--- Code/test.cpp
+++ Code/test.cpp
@@ -1,31 +1,15 @@
 #include <iostream>
-#include <cstring>
-#include "human.h"
-#include "superhero.h"
 using namespace std;
 
-void strcpy_s(char* dest, size_t dest_size, const char* src) {
-    if (dest == nullptr || src == nullptr || dest_size == 0) {
-        // 参数错误
-        return;
-    }
+double a[] = {10.1, 12.6, 33.1, 24.1, 50.0};
 
-    // 复制字符串
-    size_t src_size = strlen(src);
-    if (src_size >= dest_size) {
-        // 目标缓冲区太小
-        dest[0] = '\0';
-        return;
-    }
-    strcpy(dest, src);
+double& set(int i) {
+    return a[i];
 }
+
 
 int main() {
-    superhero IronMan;
-    cout << IronMan.Height << endl;
-
-    IronMan.info();
-    cout << IronMan.PowerType << endl;
-
-    IronMan.SuitOn(8, 10);
+    
 }
+
+
