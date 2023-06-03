d90d452becea8cd7f5fa7af06a40ef28009d5f5f
KanaMeisa <stewartjobs@outlook.com>
2023-06-03 18:41:18 +0800

    add new code

--- Code/test.cpp
+++ Code/test.cpp
@@ -9,7 +9,26 @@ double& set(int i) {
 
 
 int main() {
+    cout << "Original Data:" << endl;
     
+    for (int i = 0; i < 5; i++) {
+        cout << "a[" << i << "] = ";
+        cout << a[i] << endl; 
+    }
+
+
+    cout << "After changing:" << endl;
+
+    set(1) = 20.23;
+    set(2) = 70.8;
+
+    for (int i = 0; i < 5; i++) {
+        cout << "a[" << i << "] = ";
+        cout << a[i] << endl; 
+    }
+
+
+    return 0;
 }
 
 
