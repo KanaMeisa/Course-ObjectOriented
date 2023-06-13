4850324617497b96bc3826933cfaf0e0992a9b9f
KanaMeisa <stewartjobs@outlook.com>
2023-06-13 09:33:12 +0800

    Update note

--- Code/test.cpp
+++ Code/test.cpp
@@ -1,34 +1,27 @@
 #include <iostream>
 using namespace std;
 
-double a[] = {10.1, 12.6, 33.1, 24.1, 50.0};
 
-double& set(int i) {
-    return a[i];
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+
+void sort_descending(int *a, int *b, int *c) {
+    if (*a < *b) {
+        swap(a, b);
+    }
+    if (*a < *c) {
+        swap(a, c);
+    }
+    if (*b < *c) {
+        swap(b, c);
+    }
 }
 
 
 int main() {
-    cout << "Original Data:" << endl;
-    
-    for (int i = 0; i < 5; i++) {
-        cout << "a[" << i << "] = ";
-        cout << a[i] << endl; 
-    }
 
-
-    cout << "After changing:" << endl;
-
-    set(1) = 20.23;
-    set(2) = 70.8;
-
-    for (int i = 0; i < 5; i++) {
-        cout << "a[" << i << "] = ";
-        cout << a[i] << endl; 
-    }
-
-
-    return 0;
 }
-
-
